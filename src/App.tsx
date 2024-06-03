4946df0a776840d2de0f369a090b0c727290ae9f
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2024-06-03 14:32:02 +0300

    Clean up ignored files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import tw from 'tailwind-styled-components'
-import './App.css';
+import tw from "tailwind-styled-components"
+import "./App.css"
 
-const Container = tw.div`
+interface Props {
+	$primary: boolean
+}
+const Container = tw.div<Props>`
     flex
     items-center
     justify-center
     flex-col
     w-full
-    bg-indigo-600
+    ${(p) =>
+			p.$primary ? "bg-indigo-600" : "bg-indigo-300"}
     text-3xl
     font-bold
     text-white
 `
 
 function App() {
-  return (
-    <Container>
-        <div>Use the Container as any other React Component</div>
-    </Container>
-  );  
+	return (
+		<Container $primary={false}>
+			<div>
+				Use the Container as any other React Component
+			</div>
+		</Container>
+	)
 }
 
-export default App;
+export default App
